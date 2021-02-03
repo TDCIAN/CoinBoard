7e7f71d945a9e724e2af14a5cfce3e5c8afd1edc
Jeongmin <tdcian71@gmail.com>
2021-02-03 11:09:45 +0900

    첫번째 탭에 콜렉션뷰와 테이블뷰 생성 및 각각 셀 추가

--- CoinBoard/Chart/ChartListViewController.swift
+++ CoinBoard/Chart/ChartListViewController.swift
@@ -8,22 +8,61 @@
 import UIKit
 
 class ChartListViewController: UIViewController {
-
+    
+    @IBOutlet weak var chartCollectionView: UICollectionView!
+    @IBOutlet weak var chartTableView: UITableView!
+    @IBOutlet weak var chartTableViewHeight: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ChartListViewController {
+    private func adjustTableViewHeight() {
+        chartTableViewHeight.constant = chartTableView.contentSize.height
     }
-    */
+    
+    private func showDetail() {
+        let storyboard = UIStoryboard(name: "Chart", bundle: .main)
+        if let detailVC = storyboard.instantiateViewController(withIdentifier: "ChartDetailViewController") as? ChartDetailViewController {
+            navigationController?.pushViewController(detailVC, animated: true)
+        }
+    }
+    
+}
 
+extension ChartListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChartCardCell", for: indexPath) as? ChartCardCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+}
+
+class ChartCardCell: UICollectionViewCell {
+    
+}
+
+extension ChartListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChartListCell", for: indexPath) as? ChartListCell else { return UITableViewCell() }
+        cell.backgroundColor = .systemTeal
+        return cell
+    }
+}
+
+class ChartListCell: UITableViewCell {
+    
 }
