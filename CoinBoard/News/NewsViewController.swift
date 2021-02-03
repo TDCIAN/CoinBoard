9b4183f52ed511e9dc32266fd4fb46b4af16537a
Jeongmin <tdcian71@gmail.com>
2021-02-03 11:15:08 +0900

    NewsList TableView 세팅

--- CoinBoard/News/NewsViewController.swift
+++ CoinBoard/News/NewsViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class NewsViewController: UIViewController {
 
+    @IBOutlet var newsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension NewsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsListCell", for: indexPath) as? NewsListCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    
+}
 
+class NewsListCell: UITableViewCell {
+    
 }
