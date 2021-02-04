b4b9ed5a1bee851aed7e3786908d1bd1026e8a9f
Jeongmin <tdcian71@gmail.com>
2021-02-04 11:09:57 +0900

    Model 구성 요소 추가

---
+++ CoinBoard/Model/Article.swift
@@ -0,0 +1,8 @@
+//
+//  Article.swift
+//  CoinBoard
+//
+//  Created by APPLE on 2021/02/04.
+//
+
+import Foundation
---
+++ CoinBoard/Model/Period.swift
@@ -0,0 +1,8 @@
+//
+//  Period.swift
+//  CoinBoard
+//
+//  Created by APPLE on 2021/02/04.
+//
+
+import Foundation
