7c29b2b504f3b1d97ee3d0a2d6426d4cc1437440
Jeongmin <tdcian71@gmail.com>
2021-02-05 11:56:07 +0900

    ChartListViewController 뷰모델 추가

--- CoinBoard/Chart/ChartListViewController.swift
+++ CoinBoard/Chart/ChartListViewController.swift
@@ -8,18 +8,27 @@
 import UIKit
 import Charts
 
+typealias CoinInfo = (key: CoinType, value: Coin)
+
 class ChartListViewController: UIViewController {
     
     @IBOutlet weak var chartCollectionView: UICollectionView!
     @IBOutlet weak var chartTableView: UITableView!
     @IBOutlet weak var chartTableViewHeight: NSLayoutConstraint!
     
+    var viewModel: ChartListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel = ChartListViewModel(changeHandler: { coinInfos in
+            DispatchQueue.main.async {
+                self.chartCollectionView.reloadData()
+                self.chartTableView.reloadData()
+                self.adjustTableViewHeight()
+            }
+        })
+        viewModel.fetchData()
     }
-
 }
 
 extension ChartListViewController {
@@ -27,9 +36,10 @@ extension ChartListViewController {
         chartTableViewHeight.constant = chartTableView.contentSize.height
     }
     
-    private func showDetail() {
+    private func showDetail(coinInfo: CoinInfo) {
         let storyboard = UIStoryboard(name: "Chart", bundle: .main)
         if let detailVC = storyboard.instantiateViewController(withIdentifier: "ChartDetailViewController") as? ChartDetailViewController {
+//            detailVC.viewModel
             navigationController?.pushViewController(detailVC, animated: true)
         }
     }
@@ -38,11 +48,12 @@ extension ChartListViewController {
 
 extension ChartListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return viewModel.numberOfCoinInfoList
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChartCardCell", for: indexPath) as? ChartCardCell else { return UICollectionViewCell() }
+//        cell.viewModel
         return cell
     }
     
@@ -57,22 +68,18 @@ class ChartCardCell: UICollectionViewCell {
 
 extension ChartListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return viewModel.numberOfCoinInfoList
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChartListCell", for: indexPath) as? ChartListCell else { return UITableViewCell() }
-        cell.backgroundColor = .systemTeal
+        let cell = viewModel.cell(for: indexPath, at: tableView)
         return cell
     }
 }
 
-class ChartListCell: UITableViewCell {
-    @IBOutlet weak var currentStatusBox: UIView!
-    @IBOutlet weak var coinName: UILabel!
-    @IBOutlet weak var currentPrice: UILabel!
-    @IBOutlet weak var change24Hours: UILabel!
-    @IBOutlet weak var changePercent: UILabel!
-    @IBOutlet weak var currency: UILabel!
-    @IBOutlet weak var currentStatusImageView: UIImageView!
+extension ChartListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showDetail(coinInfo: viewModel.coinInfo(at: indexPath))
+    }
 }
+
---
+++ CoinBoard/Chart/ChartListViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  ChartListViewModel.swift
+//  CoinBoard
+//
+//  Created by APPLE on 2021/02/05.
+//
+
+import UIKit
+
+class ChartListViewModel {
+    typealias Handler = ([CoinInfo]) -> Void
+    var changeHandler: Handler
+    
+    var coinInfoList: [CoinInfo] = [] {
+        didSet {
+            changeHandler(coinInfoList)
+        }
+    }
+    init(changeHandler: @escaping Handler) {
+        self.changeHandler = changeHandler
+    }
+}
+
+extension ChartListViewModel {
+    func fetchData() {
+        NetworkManager.requestCoinList { result in
+            switch result {
+            case .success(let coins):
+                let tuples = zip(CoinType.allCases, coins).map { (key: $0, value: $1) }
+                self.coinInfoList = tuples
+            case .failure(let error):
+                print("--> coin list error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    var numberOfCoinInfoList: Int {
+        return coinInfoList.count
+    }
+    
+    func cell(for indexPath: IndexPath, at tableView: UITableView) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChartListCell", for: indexPath) as? ChartListCell else { return UITableViewCell()
+        }
+        let coinInfo = coinInfoList[indexPath.row]
+        cell.configCell(coinInfo: coinInfo)
+        return cell
+    }
+    
+    func coinInfo(at indexPath: IndexPath) -> CoinInfo {
+        let coinInfo = coinInfoList[indexPath.row]
+        return coinInfo
+    }
+}
