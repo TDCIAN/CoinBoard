546d61fb3dc307ac8a7b3fdecf0bc19eed53293b
Jeongmin <tdcian71@gmail.com>
2021-02-09 21:36:34 +0900

    원복

--- CoinBoard/LaunchScreenView.swift
+++ CoinBoard/LaunchScreenView.swift
@@ -5,4 +5,15 @@
 //  Created by APPLE on 2021/02/09.
 //
 
-import Foundation
+import UIKit
+
+class LaunchScreenView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
