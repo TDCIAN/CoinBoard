54fb6a5645c6c25098b5e8aa0b979801e734d966
Jeongmin <tdcian71@gmail.com>
2021-02-11 11:18:57 +0900

    정리

--- CoinBoard/Network/Request.swift
+++ CoinBoard/Network/Request.swift
@@ -60,7 +60,7 @@ extension Request {
         case .body(let params):
             let bodyData = try? JSONSerialization.data(withJSONObject: params, options: [])
             if let data = bodyData {
-                print("Request - httpBody: \(data)")
+//                print("Request - httpBody: \(data)")
                 request.httpBody = data
             }
         case .url(let params):
@@ -68,7 +68,7 @@ extension Request {
             var components = URLComponents(string: path)
             components?.queryItems = queryParams
             request.url = components?.url
-            print("Request - components: \(components), request.url: \(components?.url)")
+//            print("Request - components: \(components), request.url: \(components?.url)")
         }
         print("Request - return 되는 request: \(request)")
         return request
